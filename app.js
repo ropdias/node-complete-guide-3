afea24444d701c533bd556c3629b4f47a1418efd
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2022-07-11 03:44:24 -0300

    Using body-parser to parsing incoming requests

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.get("/favicon.ico", (req, res) => res.sendStatus(204));
-// For handling the favicon.ico in multiple ways you can check this stackoverflow question:
-// https://stackoverflow.com/questions/35408729/express-js-prevent-get-favicon-ico
-// app.use((req, res, next) => {
-//   if (req.originalUrl && req.originalUrl.split("/").pop().includes('favicon')) {
-//     return res.sendStatus(204);
-//   }
-//   return next();
-// });
-
-app.use("/", (req, res, next) => {
-  console.log("This always runs!");
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("This always runs too!");
-  next();
-});
 
 app.use("/add-product", (req, res, next) => {
   console.log("In 'add-product' middleware!");
-  res.send("<h1>The 'Add Product' Page!</h1>");
+  res.send("<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>");
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use("/", (req, res, next) => {
@@ -32,6 +22,4 @@ app.use("/", (req, res, next) => {
   res.send("<h1>Hello from express!</h1>");
 });
 
-app.listen(3000); // This do the same as the next 2 lines:
-// const server = http.createServer(app);
-// server.listen(3000);
+app.listen(3000);
