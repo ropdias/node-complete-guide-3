0c4a5bb5ca9539963295e5f682cceecbb4f2fa5f
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2022-07-16 13:45:29 -0300

    Assignment 3 and final version

--- routes/shop.js
+++ routes/shop.js
@@ -2,6 +2,9 @@ const path = require("path");
 
 const express = require("express");
 
+// You use rootDir here because you can't get an absolute path to the 
+// folder of the app.js file if you use path.join(__dirname, "views", "shop.html") directly here
+// You could use path.join(__dirname, '..', "views", "shop.html")
 const rootDir = require("../util/path");
 
 const router = express.Router();
